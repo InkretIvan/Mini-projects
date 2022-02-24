15fea676f863a9871b3dc0c7a5123983b6d6c06a
InkretIvan <ivaninkret7@gmail.com>
2022-02-24 19:54:14 +0100

    /cleaning desktop git

---
+++ game1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+    
+    return 0;
+}
---
+++ java swing revision/HelloWorld.java
@@ -0,0 +1,12 @@
+ public class HelloWorld { 
+   public static void pom(){
+      System.out.println("Hello world");
+      System.out.println("what");
+      }
+   public static void main(String[] args){
+     System.out.println("Hello world");
+     System.out.println("what");
+     pom();
+     }
+
+    }
